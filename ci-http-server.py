61ecc8c4f51087f0423f3eabe02adce9fb1f4eaf
John Carr <john.carr@isotoma.com>
2012-10-14 20:37:10 +0100

    Hack around CI

--- ci-http-server.py
+++ ci-http-server.py
@@ -1,10 +1,21 @@
 import os
 import sys
+import signal
+import time
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 if os.fork() == 0:
-    if os.fork() == 0:
-        httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', int(sys.argv[1])), SimpleHTTPRequestHandler)
-        httpd.handle_request()
+    child2 = os.fork()
 
+    if child2 == 0:
+        httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', int(sys.argv[1])), SimpleHTTPRequestHandler)
+        try:
+            httpd.handle_request()
+        except KeyboardInterrupt:
+            pass
+
+    if os.fork() == 0:
+        time.sleep(10)
+        os.kill(child2, signal.SIGINT)
+    
